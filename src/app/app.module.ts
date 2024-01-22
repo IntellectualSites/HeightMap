0ad18aaef264e4896025709dfe99d854502ec277
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-01-22 14:31:15 +0100

    Add main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import {ClipboardModule} from "@angular/cdk/clipboard";
+import { HeightMapImageComponent } from './components/height-map-image/height-map-image.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeightMapImageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClipboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
