136e0dcb4187cf225d1fe58c7319c11c7f9a1bf4
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-01-22 14:31:10 +0100

    Add service

--- src/app/services/ImageService/image.service.ts
+++ src/app/services/ImageService/image.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  assetsFolder = "assets"
+  private assetsFolder: string = environment.imageFolder
+  private fawePath: string = environment.basePath;
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
 
 
-  getImage(fileName: string): Observable<Object> {
-    return this.http.get(`${this.assetsFolder}/${fileName}`);
+  getImageUrl(fileName: string): string {
+    return `${this.assetsFolder}/${fileName}`;
+  }
+
+  getFilePath(fileName: string): string {
+    return this.fawePath.replace(environment.replaceString, fileName);
   }
 
 
