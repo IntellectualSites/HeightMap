1815bebd56025661154e809f8aa67c216f16f092
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-01-22 14:30:57 +0100

    Add environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  production: false,
+  command: '//brush heightmap "%image%"',
+  imageFolder: 'assets/images',
+  replaceString: '%image%',
+  basePath: 'images/min/%image%',
+  title: "Height Map Interface"
+};
